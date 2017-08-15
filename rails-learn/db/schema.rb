61ceea73303a9298710377efa67f29769aef04ab
zhirong.li <zhirong.li@alo7.com>
2017-08-15 19:13:09 +0800

    添加micropost的模型

--- rails-learn/db/schema.rb
+++ rails-learn/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815072622) do
+ActiveRecord::Schema.define(version: 20170815104850) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
