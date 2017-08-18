7e2023014b17b0641b8df6be764d8ab2480cbb14
zhirong.li <zhirong.li@alo7.com>
2017-08-18 18:53:47 +0800

    添加show 页面

--- rails-learn/app/controllers/users_controller.rb
+++ rails-learn/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
   end
+  def show
+    @user = User.find(params[:id]) 
+    #debugger #debugger 方法加到应用中
+  end
 end
--- rails-learn/config/routes.rb
+++ rails-learn/config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users #为演示应用中的 Users 资源提供了符合 REST 架构的所有动作,以及用来获取相应 URL 的具名路由
 end
