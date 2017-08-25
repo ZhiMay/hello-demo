0e9ee9c4e207d5dcb63f45b2004bf9b44ee5596e
zhirong.li <zhirong.li@alo7.com>
2017-08-25 18:59:35 +0800

    更新

--- rails-learn/app/controllers/users_controller.rb
+++ rails-learn/app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def new
+    #debugger
+    @user = User.new;
+
   end
   def show
     @user = User.find(params[:id]) 
-    #debugger #debugger 方法加到应用中
+    debugger
+    #debugger #debugger 方法加到应用中,Rails 服务器的输出中会显示 byebug 提示符
   end
 end
